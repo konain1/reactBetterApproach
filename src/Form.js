e9324b403918b51cd9fed7b1837ee097c803a9d9
Konain1 <kaunainkasab@gmail.com>
2023-10-24 23:20:54 +0530

    done

--- src/Form.js
+++ src/Form.js
@@ -1,24 +1,36 @@
 import { useState } from "react"
 import "./Form.css"
 function Form(){
-    const [user,setUser] = useState({email:"",password:""})
+    const [user,setUser] = useState(
+        {email:"",
+        password:"",
+        gender:""
+    })
 
     const submitHandler = (e)=>{
-        e.preventDefault()
-
-        console.log(user.email , user.password)
-      
+        e.preventDefault();
+        console.log(user)
+       
     }
+    const changeHandler = (e)=>{
+        setUser((prev)=> ({...prev,[e.target.name]:e.target.value}))
 
+    }
+    
     return(
         <div >
 
         <form className="form">
         <label htmlFor="email">email</label>
-        <input type="email" id="email" placeholder="email" required  onChange={(e)=>(setUser((prev)=>({...prev,email:e.target.value})))}/>
+        <input type="email" id="email" placeholder="email" name="email" required  onChange={(e)=>(setUser((prev)=>({...prev,email:e.target.value})))}/>
         <br></br>
         <label htmlFor="password">password</label>
-        <input type="password" id="password" placeholder="password" required onChange={(e)=>(setUser((prev)=>({...prev,password:e.target.value})))} />
+        <input type="password" id="password" name="password" placeholder="password" required onChange={(e)=>(setUser((prev)=>({...prev,password:e.target.value})))} />
+        <br></br>
+        <label htmlFor="gender">Gender</label>
+        <input type="radio" id="male"  name="gender" value="male" onChange={(e)=>changeHandler(e)} />
+        <input type="radio" id="female" name="gender" value="female"  onChange={(e)=>changeHandler(e)} />
+
         <br></br>
         <button type="submit" id="btn" onClick={submitHandler}>submit</button>
         </form>
