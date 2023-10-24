f8b7af4e5d5e95cbb59424851ae9e1cbc9d0b3e7
Konain1 <kaunainkasab@gmail.com>
2023-10-24 13:22:13 +0530

    css added

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import "./Form.css"
 function Form(){
     const [user,setUser] = useState({email:"",password:""})
 
@@ -6,19 +7,20 @@ function Form(){
         e.preventDefault()
 
         console.log(user.email , user.password)
+      
     }
 
     return(
         <div >
 
         <form className="form">
-        <label htmlFor="useremail">email</label>
-        <input type="email" id="htmlFor" placeholder="email" required  onChange={(e)=>(setUser((prev)=>({...prev,email:e.target.value})))}/>
+        <label htmlFor="email">email</label>
+        <input type="email" id="email" placeholder="email" required  onChange={(e)=>(setUser((prev)=>({...prev,email:e.target.value})))}/>
         <br></br>
         <label htmlFor="password">password</label>
         <input type="password" id="password" placeholder="password" required onChange={(e)=>(setUser((prev)=>({...prev,password:e.target.value})))} />
         <br></br>
-        <button type="submit" onClick={submitHandler}>submit</button>
+        <button type="submit" id="btn" onClick={submitHandler}>submit</button>
         </form>
        
         </div>
